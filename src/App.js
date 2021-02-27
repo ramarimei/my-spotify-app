a5d34035dc539357dc4638279316130c8b013979
Ramari Heperi <ramarimei@Ramaris-MacBook-Air.local>
2021-02-27 23:18:02 +1300

    commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,53 @@
 import React, { Component } from "react";
 import "./App.css";
 
-let defaultTextColor = '#fff';
+let defaultTextColor = "#fff";
 
 class Aggregate extends Component {
   render() {
     return (
-      <div style={{width: "40%", display: 'inline-block'}}>
-        <h2 style={{color: defaultTextColor}}>Number Text</h2>
+      <div style={{ width: "40%", display: "inline-block" }}>
+        <h2 style={{ color: defaultTextColor }}>Number Text</h2>
       </div>
     );
   }
 }
 
-class Filter extends Component{
-  render (){
+class Filter extends Component {
+  render() {
     return (
-        <div style={{color: defaultTextColor}}>
-          <img/>
-        <input type="text"/>
-        Filter
-        </div>
+      <div style={{ color: defaultTextColor }}>
+        <img />
+        <input type="text" />
+        <img />
+      </div>
     );
   }
 }
-class App extends Component {
+class PlayList extends Component {
+  render() {
+    return (
+      <div style={{ color: defaultTextColor }}>
+        <img />
+        <h3>PlayList Name</h3>
+        <ul>
+          <li>Song 1</li>
+          <li>Song 2</li>
+          <li>Song 3</li>
+        </ul>
+      </div>
+    );
+  }
+}
 
+class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Title</h1>
-      <Aggregate/>
-      <Aggregate/>
-      <Filter/>
+        <Aggregate /> <Aggregate />
+        <Filter />
+        <PlayList />
       </div>
     );
   }
