93229cf35ffc44d403740556ce269d2e66dc3af9
Ramari Heperi <ramarimei@Ramaris-MacBook-Air.local>
2021-02-27 22:45:44 +1300

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
 
+let textColor = '#fff';
+
 class Aggregate extends Component {
   render() {
     return (
@@ -11,17 +13,26 @@ class Aggregate extends Component {
   }
 }
 
-
-
-
+class Filter extends Component{
+  render (){
+    return (
+        <div>
+          <img/>
+        <input type="text"/>
+        Filter
+        </div>
+    );
+  }
+}
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-      <App/>
-      <h1>Title</h1>
+        <h1>Title</h1>
       <Aggregate/>
       <Aggregate/>
+      <Filter/>
       </div>
     );
   }
